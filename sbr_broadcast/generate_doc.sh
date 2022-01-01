87238aa384ffce1e1ca2e2470187548722bfc64e
Luca Joss <luca.joss@arcanite.ch>
2022-01-01 13:40:39 +0100

    Fixed github Pages for Rust documentation

--- sbr_broadcast/generate_doc.sh
+++ sbr_broadcast/generate_doc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cargo doc --no-deps
-rm -rf ./docs
+rm -rf ../docs
 echo "<meta http-equiv=\"refresh\" content=\"0; url=sbr_broadcast\">" > target/doc/index.html
-cp -r target/doc ./docs
+cp -r target/doc ../docs
